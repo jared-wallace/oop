26afff49b4657044ab4f98af4e28db7eaea4e96f
Mason Egger <mason@cs.txstate.edu>
2014-08-29 07:14:17 -0500

    fixed formatting

--- Asg1.java
+++ Asg1.java
@@ -13,39 +13,38 @@ public class Asg1
 {    
     public static void main(String[] args) 
     {    	
-		ArrayList<Car> ray = new ArrayList<Car>();
+      ArrayList<Car> ray = new ArrayList<Car>();
 		
 		//test case
-		ray.add(new Car("Stuff","Toyota","Corola",2006,554.5));    	
+      ray.add(new Car("Stuff","Toyota","Corola",2006,554.5));    	
 			
-    	Scanner kb = new Scanner(in);
+      Scanner kb = new Scanner(in);
     	boolean loopControl = true;
     	while(loopControl)
     	{
-	    	printMenu();
-	    	switch(kb.nextInt())
-	    	{
-	    		case 1: showCars(ray); break;
-	    		case 2: if(addCar(ray, kb))
-	    				{
-	    					out.println("Car was added successfully");
-	    				} 
-	    				else
-	    					out.println("Car was not added successfully");
-	    					break;
+         printMenu();
+         switch(kb.nextInt())
+         {
+	    	   case 1: showCars(ray); break;
+	    	   case 2: if(addCar(ray, kb))
+	    		        {
+                       out.println("Car was added successfully");
+                    } 
+                    else
+                       out.println("Car was not added successfully");
+                       break;
 	    		case 3:if(deleteCar(ray, kb))
-	    				{
-	    					out.println("Car was added successfully");
-	    				} 
-	    				else
-	    					out.println("Car was not added successfully");
-	    					break;
+                   {
+                      out.println("Car was added successfully");
+                   }   
+                   else
+                      out.println("Car was not added successfully");
+                      break;
 	    	    case 4:
 	    	    case 5:
-	    		case 6: loopControl = false; out.println("bye"); break;
-	    	}    	
-    	}
-
+             case 6: loopControl = false; out.println("bye"); break;
+         }    	
+      }
     }
     
     //haven't decided if these should be in Main or in a Management class
--- Car.java
+++ Car.java
@@ -1,34 +1,35 @@
 
-public class Car {
+public class Car 
+{
 
-	private String lpNumber, make, model;
-	private int year;
-	private double price;
+   private String lpNumber, make, model;
+   private int year;
+   private double price;
 	
-    public Car(String lpNumber, String make, String model, int year, double price) 
-    {
-    	if(lpNumber == "" || make == "" || model == "" || year < 0 || price < 0)
-    	{
-    		//Validation	
-    	}
-    	this.lpNumber = lpNumber;
-    	this.make = make;
-    	this.model = model;
-    	this.year = year;
-    	this.price = price;
-    }
+   public Car(String lpNumber, String make, String model, int year, double price) 
+   {
+      if(lpNumber == "" || make == "" || model == "" || year < 0 || price < 0)
+      {
+   	   //Validation	
+      }
+      this.lpNumber = lpNumber;
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.price = price;
+   }
     
-    //Should we allow default init?
-    public Car()
-    {
-    	this("XXX-XXX","Empty","Empty",0,0.0);
-    }
+   //Should we allow default init?
+   public Car()
+   {
+      this("XXX-XXX","Empty","Empty",0,0.0);
+   }
     
-    //didn't do anything specital with formatting. Do that later
-    public String toString()
-    {
-    	return lpNumber + " " + make + " " + model + " " + year + " " + price;
-    }
+   //didn't do anything specital with formatting. Do that later
+   public String toString()
+   {
+      return lpNumber + " " + make + " " + model + " " + year + " " + price;
+   }
     
     
 }
