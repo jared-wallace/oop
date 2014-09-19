50b412dfc9d343a7f297df659f5944413e18bc6c
Jared Wallace <jared@jared-wallace.com>
2014-09-19 09:29:06 -0500

    Added and encapsulated maxLoadWeight and lengthFT in Truck class

--- resources/CarInventoryManagement/Truck.java
+++ resources/CarInventoryManagement/Truck.java
@@ -5,9 +5,28 @@ package resources.CarInventoryManagement;
  */
 public class Truck extends Vehicle {
 
+    private int maxLoadWeight = 0;
+    private double lengthFT = 0;
+
     public Truck(int maxLoadWeight, float lengthFT, String vin, String make, String model, float price, int mileage, int year) {
         super(vin,make, model, price, mileage, year);
-        int maxLoadWeight1 = maxLoadWeight;
-        float lengthFT1 = lengthFT;
+        this.setMaxLoadWeight(maxLoadWeight);
+        this.setLengthFT(lengthFT);
+    }
+
+    public int getMaxLoadWeight() {
+        return maxLoadWeight;
+    }
+
+    public void setMaxLoadWeight(int maxLoadWeight) {
+        this.maxLoadWeight = maxLoadWeight;
+    }
+
+    public double getLengthFT() {
+        return lengthFT;
+    }
+
+    public void setLengthFT(float lengthFT) {
+        this.lengthFT = lengthFT;
     }
 }
